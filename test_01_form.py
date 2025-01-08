f39f48a1da3f8419b1f5be1c91fbea0f2b1b5f92
AlbertBX <avh1972@mail.ru>
2025-01-08 13:09:13 +0500

    Update test_01_form.py

--- test_01_form.py
+++ test_01_form.py
@@ -2,51 +2,54 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-input_field = driver.find_element(By.NAME, 'first-name')
-input_field.send_keys("Иван")
+def test_1(driver):
 
-input_field = driver.find_element(By.NAME, 'last-name')
-input_field.send_keys("Петров")
+    driver = webdriver.Chrome()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+               )
 
-input_field = driver.find_element(By.NAME, 'address')
-input_field.send_keys("Ленина, 55-3")
+    input_field = driver.find_element(By.NAME, 'first-name')
+    input_field.send_keys("Иван")
 
-input_field = driver.find_element(By.NAME, 'e-mail')
-input_field.send_keys("test@skypro.com")
+    input_field = driver.find_element(By.NAME, 'last-name')
+    input_field.send_keys("Петров")
 
-input_field = driver.find_element(By.NAME, 'phone')
-input_field.send_keys("+7985899998787")
+    input_field = driver.find_element(By.NAME, 'address')
+    input_field.send_keys("Ленина, 55-3")
 
-input_field = driver.find_element(By.NAME, 'city')
-input_field.send_keys("Москва")
+    input_field = driver.find_element(By.NAME, 'e-mail')
+    input_field.send_keys("test@skypro.com")
 
-input_field = driver.find_element(By.NAME, 'country')
-input_field.send_keys("Россия")
+    input_field = driver.find_element(By.NAME, 'phone')
+    input_field.send_keys("+7985899998787")
 
-input_field = driver.find_element(By.NAME, 'job-position')
-input_field.send_keys("QA")
+    input_field = driver.find_element(By.NAME, 'city')
+    input_field.send_keys("Москва")
 
-input_field = driver.find_element(By.NAME, 'company')
-input_field.send_keys("SkyPro")
+    input_field = driver.find_element(By.NAME, 'country')
+    input_field.send_keys("Россия")
 
-blue_button = driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-'
-                                  'outline-primary mt-3"]')
+    input_field = driver.find_element(By.NAME, 'job-position')
+    input_field.send_keys("QA")
 
-blue_button.click()
+    input_field = driver.find_element(By.NAME, 'company')
+    input_field.send_keys("SkyPro")
 
-assert "danger" in driver.find_element(By.ID, "zip-code"
-                                      ).get_attribute("class")
+    blue_button = driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-'
+                                      'outline-primary mt-3"]')
 
-fields = ['first-name', 'last-name', 'address', 'e-mail', 'phone', 'city',
-          'country', 'job-position', 'company']
+    blue_button.click()
 
-for field in fields:
-    assert "success" in driver.find_element(By.ID, field
+    assert "danger" in driver.find_element(By.ID, "zip-code"
                                            ).get_attribute("class")
 
+    fields = ['first-name', 'last-name', 'address', 'e-mail', 'phone', 'city',
+              'country', 'job-position', 'company']
 
-driver.quit()
+    for field in fields:
+        assert "success" in driver.find_element(By.ID, field
+                                                ).get_attribute("class")
 
+    driver.quit()
+  

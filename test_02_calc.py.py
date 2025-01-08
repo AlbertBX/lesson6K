5a611435558d1591f828b0e65adc8b7b9a3b6028
AlbertBX <avh1972@mail.ru>
2025-01-08 09:15:07 +0500

    Update test_02_calc.py.py

--- test_02_calc.py.py
+++ test_02_calc.py.py
@@ -1,26 +1,28 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.weit import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 driver = webdriver.Chrome()
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+driver.get(
+    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-input_field = driver.find_element(By.ID,'delay')
+input_field = driver.find_element(By.ID, 'delay')
 
 input_field.clear()
 
 input_field.send_keys(45)
+
 
 driver.find_element(By.XPATH, '//span[text()="7"]').click()
 driver.find_element(By.XPATH, '//span[text()="+"]').click()
 driver.find_element(By.XPATH, '//span[text()="8"]').click()
 driver.find_element(By.XPATH, '//span[text()="="]').click()
 
-result = WebDriverWait (driver, 45). until(EC.element_to_be_clickable ((By.CSS_SELECTOR,'div[class="screen"]'))).text
-print (result)
-assert result=="15"
+result = WebDriverWait(driver, 45).until(EC.presence_of_element_located(
+    (By.CSS_SELECTOR, 'div[class="screen"]'))).text
+print(result)
+assert result == "15"
 
 driver.quit()
